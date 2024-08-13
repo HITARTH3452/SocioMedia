d0a489616c565f7e01db9b23871e05627ab21180
HITARTH3452 <hitarthsingh2201@gmail.com>
2024-08-13 11:51:05 +0530

    set navbar and theme with material UI

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import { Close, DarkMode, Help, LightMode, Message, Search } from "@mui/icons-material";
+import {useTheme, Box, FormControl, IconButton, InputBase, MenuItem, Select, Typography, useMediaQuery, Menu } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { setLogout, setMode } from "state";
 
 const Navbar = () => {
+  const[isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state) => state);
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+  const theme = useTheme();
+  
   return (
     <div>
       Navbar
